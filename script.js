c83af0f93227081cca46e7c69fd7d4f9ca89f103
Wendy Wang <wendywangg928@gmail.com>
2024-05-27 18:25:45 -0400

    completed js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
 
-search.addEventListener("click", () => {
-  search.classList.add("active");
+btn.addEventListener("click", () => {
+  // toggle allows you to open and unopen so you can open or close the search bar
+  search.classList.toggle("active");
+  //   the focus will make sure that once the search bar opens the user can directly start typing instead of having to click in the box before typing
+  input.focus();
 });
